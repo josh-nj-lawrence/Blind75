37e65d68ca2941e170497d9d702a8b7875457de8
Josh Lawrence <josh.nj.lawrence@gmail.com>
2022-12-18 19:44:30 -0500

    Added timing wrapper and improved implementation

--- day1-prefixsum/blind75-1480.py
+++ day1-prefixsum/blind75-1480.py
@@ -1,6 +1,18 @@
 # Running Sum of 1d Array
+# rewritting with timing method and preallocation of memory
+from time import time as perf_counter
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print('Method ' + method.__name__ + ' took : ' +
+              "{:2.5f}".format(perf_counter()-t) + ' sec')
+        return ret
+    return wrapper_method
 
 class Solution:
+    @profiler
     def runningSum(self, nums):
         total = 0
         running_sum = []
@@ -9,5 +21,18 @@ class Solution:
             running_sum.append(total)
         return running_sum
 
+class Solution2:
+    @profiler
+    def runningSum(self, nums):
+        total = 0
+        running_sum = [0]*len(nums)
+        for i, num in enumerate(nums):
+            total += num
+            running_sum[i] = total
+        return running_sum
+
 solution = Solution()
-print(solution.runningSum([1,2,3,4,5]))
+solution2 = Solution2()
+nums = [x for x in range(100000000)]
+solution.runningSum(nums)
+solution2.runningSum(nums)
