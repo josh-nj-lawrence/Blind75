e33886112f5887a424e0171e109429dbdf5af7ad
Josh Lawrence <josh.nj.lawrence@gmail.com>
2022-12-26 18:00:38 -0500

    Q26 modified for better performance

--- extra/leetcode-26.py
+++ extra/leetcode-26.py
@@ -15,9 +15,12 @@ class Solution2:
     def removeDuplicates(self, nums: List[int]) -> int:
         init_len = len(nums)
         for i, x in enumerate(nums):
-            if i <= len(nums)-1:
-                while nums[i+1] == x:
+            keep_popping = True
+            while keep_popping and i < len(nums)-1:
+                if nums[i+1] == x:
                     nums.pop(i)
+                else:
+                    keep_popping = False
         ret_val = len(nums)
         for x in range(len(nums)+1,init_len+1):
             nums.append(0)
