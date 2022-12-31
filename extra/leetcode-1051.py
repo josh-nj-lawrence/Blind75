6a3fc9eb0bbffd9ce899981002b7536299be481a
Josh Lawrence <josh.nj.lawrence@gmail.com>
2022-12-31 14:03:55 -0500

    Q1051 Height Checker

---
+++ extra/leetcode-1051.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        out_of_order = 0
+        for i, _ in enumerate(heights):
+            if expected[i] != heights[i]:
+                out_of_order +=1 
+        return out_of_order
+
+solution = Solution()
+print(solution.heightChecker([1,3,4,2,5,6,4]))
