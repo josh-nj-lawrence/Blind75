fc5dea1346bcf0fb3416c9a87bd2ff1d1bf49000
Josh Lawrence <josh.nj.lawrence@gmail.com>
2023-01-12 23:09:50 -0500

    Q7 Modifications

--- extra/leetcode-7.py
+++ extra/leetcode-7.py
@@ -1,6 +1,10 @@
 from typing import List
 class Solution:
     def reverse(self, x: int) -> int:
-        return str(x)[::-1]
+        if x is None:
+            return None
+        elif x<0:
+            return int("-" + str(abs(x))[::-1]) if int("-" + str(abs(x))[::-1]) > (-pow(2,31)) else 0
+        return int(str(x)[::-1]) if int(str(x)[::-1]) < pow(2,31)-1 else 0
 solution = Solution()
-print(solution.reverse(123))
+print(solution.reverse(-123))
